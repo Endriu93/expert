36424643a8972ea715cc8ab8ebaca9d4d1bb49b6
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-05-27 02:39:29 +0200

    nn

---
+++ src/main/java/Smart.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import org.jdom2.*;
+/**
+ * Servlet implementation class Smart
+ */
+@WebServlet("/Smart")
+public class Smart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Smart() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+response.getWriter().println(request.getHeader("Origin"));
+		
+		response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));//
+		response.addHeader("Access-Control-Allow-Methods", "POST,GET");//
+		//response.addHeader("Access-Control-Allow-Methods", "GET,POST");//
+		response.addHeader("Access-Control-Allow-Headers", "*");//
+	    response.setHeader("Access-Control-Max-Age", "6000");
+	    response.setHeader("Access-Control-Allow-Credentials", "true");		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println(request.getHeader("Origin"));
+		
+		response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));//
+		response.addHeader("Access-Control-Allow-Methods", "POST,GET");//
+		//response.addHeader("Access-Control-Allow-Methods", "GET,POST");//
+		response.addHeader("Access-Control-Allow-Headers", "*");//
+	    response.setHeader("Access-Control-Max-Age", "6000");
+	    response.setHeader("Access-Control-Allow-Credentials", "true");
+
+
+	}
+
+}
