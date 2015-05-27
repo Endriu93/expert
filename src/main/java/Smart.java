8d7c65bb48cce385b297847d0d60faa098caa0c0
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-05-27 03:46:26 +0200

    2222222222

--- src/main/java/Smart.java
+++ src/main/java/Smart.java
@@ -1,13 +1,11 @@
 
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 //import org.jdom2.*;
 /**
  * Servlet implementation class Smart
@@ -30,7 +28,7 @@ public class Smart extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub//
-	
+	response.addHeader("Access-Control-Access-Origin", "*");
 	}
 
 	/**
@@ -38,6 +36,8 @@ public class Smart extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	response.addHeader("Access-Control-Access-Origin", "*");
+
 
 
 	}
