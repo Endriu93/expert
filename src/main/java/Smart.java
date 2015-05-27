36cefc17e43fd778773a3e3cdc030ae9faced530
Andrzej Węgrzyn <andrzej9393@gmail.com>
2015-05-27 03:49:07 +0200

    nbm

--- src/main/java/Smart.java
+++ src/main/java/Smart.java
@@ -28,6 +28,7 @@ public class Smart extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub//
+		response.getWriter().println("ooooooooooooooooo");
 	response.addHeader("Access-Control-Access-Origin", "*");
 	}
 
@@ -36,6 +37,8 @@ public class Smart extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().println("ooooooooooooooooo");
+
 	response.addHeader("Access-Control-Access-Origin", "*");
 
 
